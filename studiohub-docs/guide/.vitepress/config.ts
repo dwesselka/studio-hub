fb54a44848cfe92dc37a72956af87efd9db75210
Diego Wesselka <diegowesselka1@gmailcom>
2026-07-21 17:18:29 -0300

    Update docs structure

--- studiohub-docs/guide/.vitepress/config.ts
+++ studiohub-docs/guide/.vitepress/config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  srcDir: './docs',
   title: 'StudioHub Guide',
   description: 'Construindo um SaaS. Evoluindo um Engenheiro.',
   ignoreDeadLinks: false,
